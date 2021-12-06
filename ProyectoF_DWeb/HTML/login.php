b30d8af0991a2f3417a5412e8712013b79018005
César Antonio Vargas Rodríguez <cesar.vargas@sinergia.com.mx>
2021-12-06 04:50:11 -0600

    Estilo con Bootstrap

--- ProyectoF_DWeb/HTML/login.php
+++ ProyectoF_DWeb/HTML/login.php
@@ -10,14 +10,18 @@
         </header>
 
         <main>
-            <section class="log">
+            <section class="log container">
                 <h3>Iniciar sesión</h3><br><br>
                 <form method="post">
-                    <label for="email">Correo *</label>
-                    <input type="text" id="email" name="email" class="form-control" required placeholder="Igresa tu correo">
-                    <label for="password">Contraseña *</label>
-                    <input type="text" id="password" name="password" class="form-control" required placeholder="Pon tu contraseña">
-                    <button type="submit" class="">Entrar</button>
+                    <div class="row">
+                        <label for="email">Correo *</label>
+                        <input type="text" id="email" name="email" class="form-control" required placeholder="Igresa tu correo">
+                        <label for="password">Contraseña *</label>
+                        <input type="text" id="password" name="password" class="form-control" required placeholder="Pon tu contraseña">
+                    </div>
+                    <div class="row">
+                        <button type="submit" class="btn btn-default">Entrar</button>
+                    <div>
                 </form>
             </section>
         </main>
